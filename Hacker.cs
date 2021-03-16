9103f1760ab2a2c557880b92c854ccf92ea3a227
Xine <cjdoza@gmail.com>
2021-03-16 11:14:17 -0600

    hacker and muscle classes complete

---
+++ Hacker.cs
@@ -0,0 +1,20 @@
+using System;
+namespace ClassicHeist
+{
+    public class Hacker : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(Bank bank)
+        {
+            int HackerTalent = bank.AlarmScore - SkillLevel;
+            Console.WriteLine($"{Name} is hacking the alarm system. Decreased security by {SkillLevel}.");
+            if (HackerTalent == 0)
+            {
+                Console.WriteLine($"{Name} has cracked the safe!");
+            }
+        }
+    }
+
+}
