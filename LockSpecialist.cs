132fad8576a216f2e88ad1f552e9ff94a8a932c3
Xine <cjdoza@gmail.com>
2021-03-16 11:04:40 -0600

    lock specialist done

---
+++ LockSpecialist.cs
@@ -0,0 +1,18 @@
+using System;
+namespace ClassicHeist
+{
+    public class LockSpecialist : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(Bank bank)
+        {
+            int LockTalent = bank.VaultScore - SkillLevel;
+            Console.WriteLine($"{Name} is picking the locks. Decreased security by {LockTalent}.");
+
+        }
+    }
+
+
+}
